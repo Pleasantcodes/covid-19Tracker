4abd43eefbfe553c9c12fd45a5ad9130e03ef0f0
Pleasant Codes <hellopleasantcodes@gmail.com>
2020-08-15 18:41:25 -0700

    set up made API fetched

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import styles from "./App.module.css";
+import { fetchData } from "./api/index";
 
-import "./App.css";
+import { Cards, Chart, CountryPicker } from "./components";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
   render() {
+    const { data } = this.state;
+
     return (
-      <div>
-        <h1>APP</h1>
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
---
+++ src/components/Cards/cards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cards = (props) => {
+  console.log(props);
+
+  [123][123][123];
+
+  return <h1>Cards</h1>;
+};
+
+export default Cards;
